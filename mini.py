71c7b372e2695a579286a621c9f8b7eb2ad3170f
f4t4nt <nishant@bhakars.com>
2023-12-02 12:46:35 -0800

    deleted ./zzz, probability.py -> main.py

--- mini.py
+++
@@ -1,7 +0,0 @@
-# read in all words from input() splitting by ' '
-words = input().split(' ')
-
-# convert each word to set and then back to string and print it
-
-for word in words:
-    print(''.join(set(word)), end=' ')
